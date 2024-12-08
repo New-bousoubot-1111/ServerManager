3dc9c7b0a6591d394400d1486f580b1d4b665d1e
New-bousoubot-1111 <127783879+New-bousoubot-1111@users.noreply.github.com>
2024-12-09 07:18:36 +0900

    Update earthquake.py

--- modules/earthquake.py
+++ modules/earthquake.py
@@ -103,7 +103,7 @@ class earthquake(commands.Cog):
         now = util.eew_now()
         if now == 0:
             return
-        res = requests.get(f"http://www.kmoni.bosai.go.jp/webservice/hypo/eew/20241209032700.json")
+        res = requests.get(f"http://www.kmoni.bosai.go.jp/webservice/hypo/eew/20241209032822.json")
         if res.status_code == 200:
             data = res.json()
             cache = load_data_from_db()  # PostgreSQLからキャッシュを取得
