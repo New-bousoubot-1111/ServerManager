8535a9aa3e6c28fa2de077ae9eaa0071da7d5f1f
New-bousoubot-1111 <127783879+New-bousoubot-1111@users.noreply.github.com>
2024-12-11 09:25:11 +0900

    Update tsunami.py

--- modules/tsunami.py
+++ modules/tsunami.py
@@ -16,7 +16,7 @@ color = nextcord.Colour(int(config['color'], 16))
 
 # 地図画像とポリゴンデータの準備
 image_path = "./images/tsunami.png"  # 地図画像のパス
-map_data_path = "./images/tsunami.png"  # Shapefileデータのパス
+map_data_path = "./images/tsunami.png.shp"  # Shapefileデータのパス
 
 # 地図データを読み込む (例: GeoPandasを使用)
 japan_map = gpd.read_file(map_data_path)
