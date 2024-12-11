2dbc2bc82cc4bce5c5f79d2051d7737ce48cf26e
New-bousoubot-1111 <127783879+New-bousoubot-1111@users.noreply.github.com>
2024-12-11 19:17:19 +0900

    Update tsunami.py

--- modules/tsunami.py
+++ modules/tsunami.py
@@ -68,15 +68,13 @@ class tsunami(commands.Cog):
                         print(f"未一致地域: {area_name} | REGION_MAPPING: {REGION_MAPPING.get(area_name, 'なし')}")
 
                 # 地図を描画
-                fig, ax = plt.subplots(figsize=(16, 18))  # サイズを大きく
+                fig, ax = plt.subplots(figsize=(14, 16))  # サイズを大きく
                 fig.patch.set_facecolor('#2a2a2a')  # 全体の背景色
                 ax.set_facecolor("#2a2a2a")  # 地図の背景色を薄い灰色に設定
 
                 # 地図描画
                 gdf.plot(ax=ax, color=gdf["color"], edgecolor="black", linewidth=0.5)
 
-                ax.set_xlim(122, 153)  # 経度の範囲 (日本列島全体に合わせる)
-                ax.set_ylim(20, 46)    # 緯度の範囲 (日本列島全体に合わせる)
                 # 軸を非表示にする
                 ax.set_axis_off()
 
