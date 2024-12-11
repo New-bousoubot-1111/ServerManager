b715d5e6f615ca8c72bf196a6752ce9178f10a77
New-bousoubot-1111 <127783879+New-bousoubot-1111@users.noreply.github.com>
2024-12-11 19:30:15 +0900

    Update tsunami.py

--- modules/tsunami.py
+++ modules/tsunami.py
@@ -75,10 +75,6 @@ class tsunami(commands.Cog):
                 # 地図描画
                 gdf.plot(ax=ax, color=gdf["color"], edgecolor="black", linewidth=0.5)
 
-                # 日本列島とその周辺の島々が大きく表示されるように範囲を調整
-                ax.set_xlim(130, 150)  # 経度を広げ、より大きく表示
-                ax.set_ylim(25, 45)    # 緯度の範囲を調整し、全体を大きく表示
-
                 # 軸を非表示にする
                 ax.set_axis_off()
 
