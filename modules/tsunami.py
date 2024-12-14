1a70dc8b5f537d89bb5cd54ad0844a25ec899dd3
New-bousoubot-1111 <127783879+New-bousoubot-1111@users.noreply.github.com>
2024-12-15 01:34:29 +0900

    Update tsunami.py

--- modules/tsunami.py
+++ modules/tsunami.py
@@ -199,7 +199,10 @@ def generate_map(tsunami_alert_areas):
         ax.set_facecolor("#2a2a2a")
         ax.set_xlim([-180, 180])  # 経度の範囲
         ax.set_ylim([-90, 90])    # 緯度の範囲
-        ax.set_aspect('equal')
+        ax.set_aspect('auto')
+        print(coastline_buffer_gdf.is_valid)
+        print(gdf.is_valid)
+
 
         # 海岸線バッファを背景に描画
         coastline_buffer_gdf.plot(ax=ax, color="blue", alpha=0.5, edgecolor="none", linewidth=0, label="Coastline Buffer")
